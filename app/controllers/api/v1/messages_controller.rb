e3aac443175ab29c495dea58559bfb378fb161d4
Ijay Abby <inyangasi4@gmail.com>
2021-10-31 01:24:56 +0300

    Update controller and api

---
+++ app/controllers/api/v1/messages_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::MessagesController < ApplicationController
+    def greet
+      @message = Message.find(Message.pluck(:id).sample)
+      render json: @message, status: 200
+    end
+  end
+  
---
+++ app/controllers/greet_controller.rb
@@ -0,0 +1,6 @@
+class MainController < ApplicationController
+    def index
+      render json: 'Welcome to Nigeria, the Gaint of Africa'
+    end
+  end
+  
